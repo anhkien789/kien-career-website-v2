8842dbed65937bb4e7779203be1c54d75047c9fb
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-05-18 09:55:14 +0000

    store data in cloud database

--- database.py
+++ database.py
@@ -19,11 +19,30 @@ def load_jobs_from_db():
       jobs.append(dict(row))
     return jobs
 
+
 def load_job_from_db(id):
-  with  engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
     rows = result.mappings().all()
     if len(rows) == 0:
       return None
     else:
       return dict(rows[0])
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    conn.execute(
+      query, {
+        "job_id": job_id,
+        "full_name": data["full_name"],
+        "email": data["email"],
+        "linkedin_url": data["linkedin_url"],
+        "education": data["education"],
+        "work_experience": data["work_experience"],
+        "resume_url": data["resume_url"]
+      })
